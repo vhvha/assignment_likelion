a0fa33c4ac9dbca69d49a92a951f602efe060de4
vhvha <vhvha@korea.ac.kr>
2019-04-04 05:20:11 +0900

    Create module.py

---
+++ module.py
@@ -0,0 +1,16 @@
+class FourCal:
+    def __init__(self):
+        self.first = int(input())
+        self.second = int(input())
+
+    def add(self):
+        return self.first + self.second
+
+    def sub(self):
+        return self.first - self.second
+
+    def mul(self):
+        return self.first * self.second
+
+    def div(self):
+        return self.first / self.second
